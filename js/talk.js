e958b52d1941ffbde6b1b4911bb18b13871e10da
miaoandwang <130897759+miaoandwang@users.noreply.github.com>
2023-04-17 09:02:36 +0800

    Update talk.js

--- js/talk.js
+++ js/talk.js
@@ -3,7 +3,7 @@ new TypeIt("#lywry", {
         cursorSpeed: 1000,
         speed: 100
     })
-    .type("LY && WRY")
+    .type("MQC && WDD")
     .pause(2000)
     .delete(null, {
         delay: 500
