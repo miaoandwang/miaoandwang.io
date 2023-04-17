35e733d657a87883ed8c140b27b3cb16841e4a36
miaoandwang <130897759+miaoandwang@users.noreply.github.com>
2023-04-17 09:02:17 +0800

    Update lovetime.js

--- js/lovetime.js
+++ js/lovetime.js
@@ -12,8 +12,8 @@ function lovetime() {
     var todayHour = today.getHours()
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
-    // 修改这里时间即可 我的是2021.05.04
-    var t1 = Date.UTC(2021, 05, 04, 00, 00, 00)
+    // 修改这里时间即可 我的是2021.03.14
+    var t1 = Date.UTC(2021, 03, 14, 00, 00, 00)
     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
     var diff = t2 - t1
     var diffYears = Math.floor(diff / years)
